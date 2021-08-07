6cde415b74900f3453e5b897e451dd4182c29cc0
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2021-08-07 08:53:11 -0500

    trying to use tensorflow code to compute softmax

--- src/condor_tensorflow/activations.py
+++ src/condor_tensorflow/activations.py
@@ -23,31 +23,10 @@ def ordinal_softmax(x):
            [0.8807971 , 0.01420934, 0.10499357]], dtype=float32)>
   """
 
-  # Number of columns is the number of classes - 1
-  num_classes = x.shape[1] + 1
-
   # Convert the ordinal logits into cumulative probabilities.
-  cum_probs = tf.math.cumprod(tf.math.sigmoid(x), axis = 1)#tf.map_fn(tf.math.sigmoid, x)
+  cum_probs = tf.concat([tf.ones((x.shape[0],1),dtype=tf.float32),
+                         tf.math.cumprod(tf.math.sigmoid(x), axis = 1),
+                         tf.zeros((x.shape[0],1),dtype=tf.float32)],
+                        1)
 
-  # Create a list of tensors.
-  probs = []
-
-  # First, get probability predictions out of the cumulative logits.
-  # Column 0 is Probability that y > 0, so Pr(y = 0) = 1 - Pr(y > 0)
-  # Pr(Y = 0) = 1 - s(logit for column 0)
-  probs.append(1. - cum_probs[:, 0])
-
-
-  # For the other columns, the probability is:
-  # Pr(y = k) = Pr(y > k) - Pr(y > k - 1)
-  if num_classes > 2:
-    for val in range(1, num_classes - 1):
-      probs.append(cum_probs[:, val - 1] - cum_probs[:, val])
-
-  # Special handling of the maximum label value.
-  probs.append(cum_probs[:, num_classes - 2])
-
-  # Combine as columns into a new tensor.
-  probs_tensor = tf.concat(tf.transpose(probs), axis = 1)
-
-  return probs_tensor
+  return cum_probs[:,0:-1] - cum_probs[:,1:]
