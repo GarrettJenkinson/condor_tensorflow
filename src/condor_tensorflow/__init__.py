e333821f43e9e3f3428f8b0c287c713b862582d4
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2021-09-01 21:04:21 -0500

    update the code

--- src/condor_tensorflow/__init__.py
+++ src/condor_tensorflow/__init__.py
@@ -2,10 +2,12 @@ from .version import __version__
 
 from .loss import CondorOrdinalCrossEntropy
 from .loss import SparseCondorOrdinalCrossEntropy
+from .loss import OrdinalEarthMoversDistance
+from .loss import SparseOrdinalEarthMoversDistance
 from .metrics import OrdinalMeanAbsoluteError
 from .metrics import SparseOrdinalMeanAbsoluteError
-from .metrics import OrdinalEarthMoversDistance
-from .metrics import SparseOrdinalEarthMoversDistance
+#from .metrics import OrdinalEarthMoversDistance
+#from .metrics import SparseOrdinalEarthMoversDistance
 from .activations import ordinal_softmax
 from .labelencoder import CondorOrdinalEncoder
 
