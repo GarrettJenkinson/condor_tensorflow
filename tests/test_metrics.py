5d7a34578d5555a88bc81790c04b8f4fc2153d4c
GarrettJenkinsonMayo <Jenkinson.William@mayo.edu>
2021-09-01 21:18:57 -0500

    update tests to move EMD into loss

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,5 @@
 from condor_tensorflow.metrics import OrdinalMeanAbsoluteError
 from condor_tensorflow.metrics import SparseOrdinalMeanAbsoluteError
-from condor_tensorflow.metrics import OrdinalEarthMoversDistance
-from condor_tensorflow.metrics import SparseOrdinalEarthMoversDistance
 import pytest
 import tensorflow as tf
 
@@ -20,15 +18,3 @@ def test_SparseOrdinalMeanAbsoluteError():
     tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
 
 
-def test_OrdinalEarthMoversDistance():
-    loss = OrdinalEarthMoversDistance()
-    val = loss(tf.constant([[1., 1.]]), tf.constant([[-1., 1.]]))
-    expect = tf.constant(1.5344467)
-    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
-
-
-def test_SparseOrdinalEarthMoversDistance():
-    loss = SparseOrdinalEarthMoversDistance()
-    val = loss(tf.constant([2]), tf.constant([[-1., 1.]]))
-    expect = tf.constant(1.5344467)
-    tf.debugging.assert_near(val, expect, rtol=1e-5, atol=1e-5)
